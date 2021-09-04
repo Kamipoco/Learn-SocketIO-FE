7bcb674b25cda167ba4403195ef2b1b3bdbae42c
Van Lam <vanlam020499@gmail.com>
2021-09-04 15:10:31 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+import TextField from "@material-ui/core/TextField";
+import "./App.css";
+
+const socket = io.connect("http://localhost:9009");
 
 function App() {
+  const [state, setState] = useState({ name: "", message: "" });
+  const [chat, setChat] = useState([]);
+
+  useEffect(() => {
+    socket.on("getData", ({ name, message }) => {
+      setChat([...chat, { name, message }]);
+    });
+  });
+
+  //onTextChage
+  const onTextChage = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+
+  //onMessageSubmit
+  const onMessageSubmit = (e) => {
+    e.preventDefault();
+    const { name, message } = state;
+    socket.emit("message", { name, message });
+    setState({ name, message: "" });
+  };
+
+  //func renderChat
+  const renderChat = () => {
+    return chat.map(({ name, message }, index) => (
+      <div key={index}>
+        <h3>
+          {name}: <span>{message}</span>
+        </h3>
+      </div>
+    ));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <form onSubmit={onMessageSubmit}>
+        <h1>Messenger</h1>
+        <div className="name-field">
+          <TextField
+            name="name"
+            onChange={(e) => onTextChage(e)}
+            value={state.name}
+            label="Name"
+          />
+        </div>
+        <div>
+          <TextField
+            name="message"
+            onChange={(e) => onTextChage(e)}
+            value={state.message}
+            id="outlined-multiline-static"
+            variant="outlined"
+            label="Message"
+          />
+        </div>
+        <button>Send Message</button>
+      </form>
+      <div className="render-chat">
+        <h1>Chat Log</h1>
+        {renderChat()}
+      </div>
     </div>
   );
 }
